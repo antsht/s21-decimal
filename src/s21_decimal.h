8f8ccdcf313d531cc1a7a42a5a3d544449ae6a8c
Anton Shtern <antsht@gmail.com>
2024-06-19 11:41:46 +0300

    Note

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -102,6 +102,7 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst);
 
 // To int	
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
+/* check if exp > 0 divide by 10^exp then convert */
 
 //To float	
 int s21_from_decimal_to_float(s21_decimal src, float *dst);
